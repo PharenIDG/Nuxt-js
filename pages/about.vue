12b5cc205bcd64934f84bf1624136c105eb8c6d4
PharenIDG <pharenchorkigd2024@gmail.com>
2024-06-13 09:08:18 +0700

    1 done

---
+++ pages/about.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>About Page</h1>
+    <p>This is the about page of our Nuxt 3 application.</p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h1 {
+  color: #007bff;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>Home Page</h1>
+    <p>Welcome to our Nuxt 3 application!</p>
+    <NuxtLink to="/tailwind">go to about page</NuxtLink>
+  </div>
+</template>
